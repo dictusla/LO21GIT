24d99a7343ee00556dd74cce147b6cd61a5db010
Morenovhx <39834964+Morenovhx@users.noreply.github.com>
2018-06-04 13:14:58 +0200

    Update Automates.h
    Ajout de la classe virtuelle mère Automate et Automate1D (possibilité de mettre les constructeurs et destructeurs d'automates 1D en privé si on utilise un AutomateManager)

--- Automates.h
+++ Automates.h
@@ -10,4 +10,42 @@ public:
 
 };
 
+class Automate {
+      
+    private:
+
+    public:
+        Automate() {}
+        virtual ~Automate () = 0; 
+
+        virtual void applyTransition (Etat& currentEtat) const = 0;
+        virtual void print (std::ostream& os) const = 0;
+
+    protected:
+
+};
+ 
+class Automate1D : public Automate {
+     private:
+        static const unsigned int etatMaxNbColumn = 10;
+        static const  short unsigned int maxNbEtats = 7; // Nombre d'etat max que peuvent prendre les cellules à ne pas confondre avec l'état d'un automate entre deux transitions
+        unsigned int ruleNumber; // Regle de transition
+       
+
+    public:
+        Automate1D (unsigned int ruleN); 
+        Automate1D (const Automate1D& a);
+        virtual ~Automate1D () {}
+
+        static unsigned int getMaxNbEtat () { return maxNbEtats; }
+        static unsigned int getStateMaxNbColumn () { return etatMaxNbColumn; }
+
+        virtual void applyTransition (Etat& currentEtat) const;
+        unsigned int getRuleNumber () const{ return this->ruleNumber; }
+        void setRule (int ruleN);
+        virtual void print (std::ostream& os) const;
+
+
+    protected:    
+};
 #endif // AUTOMATES_H_INCLUDED
