565309866af18c7d939fd36db8c178fe47fdc0bd
Morenovhx <39834964+Morenovhx@users.noreply.github.com>
2018-06-04 13:52:30 +0200

    Update Automates.h
    Ajout de l'automate 2D version jeu de la vie, même remarque que pour l'automate 1D si l'on utilise un automate manager il faudra passer les constructeurs et destructeurs en privé.

--- Automates.h
+++ Automates.h
@@ -40,7 +40,7 @@ class Automate1D : public Automate {
         static unsigned int getMaxNbEtat () { return maxNbEtats; }
         static unsigned int getStateMaxNbColumn () { return etatMaxNbColumn; }
 
-        virtual void applyTransition (Etat& currentEtat) const;
+        virtual void applyTransition (Etat1D& currentEtat) const;
         unsigned int getRuleNumber () const{ return this->ruleNumber; }
         void setRule (int ruleN);
         virtual void print (std::ostream& os) const;
@@ -48,4 +48,44 @@ class Automate1D : public Automate {
 
     protected:    
 };
+
+class Automate2D : public Automate
+{
+
+    private:
+        static const short unsigned int etatMaxNbLine = 10;
+        static const short unsigned int etatMaxNbColumn = 10;
+        static const unsigned int nbToBorn = 3;
+
+        unsigned int minToNotDie;
+        unsigned int maxToNotDie;
+
+        
+
+
+
+
+    public:
+        Automate2D (unsigned int minNd, unsigned int maxNd) : minToNotDie (minNd), maxToNotDie (maxNd) { if(minNd>maxNd){throw AutomateException("Minimum value superior than maximum value");}}
+        virtual ~Automate2D () {}
+
+        bool isNotInRange(const Etat& currentEtat) const;
+
+        virtual void print (std::ostream& os) const;
+        virtual void applyTransition (Etat2D& currentEtat) const;
+        virtual bool willBeBorn (unsigned int line, unsigned int column,Etat2D& s) const;
+        virtual bool willDie (unsigned int line, unsigned int column,Etat2D& s) const;
+
+        unsigned int getMinToNotDie () const { return this->minToNotDie; }
+        unsigned int getMaxToNotDie () const { return this->maxToNotDie; }
+
+        static unsigned int getNbToBorn () { return nbToBorn; }
+        static unsigned int getEtatMaxNbLine () { return etatMaxNbLine; }
+        static unsigned int getEtatMaxNbColumn () { return etatMaxNbColumn; }
+        
+    protected:
+
+};
+
+
 #endif // AUTOMATES_H_INCLUDED
